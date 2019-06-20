cb6ca9ba32d1ea55f0fffbb8fda7a319b07a7fdd
Stas <allenwalker12349@gmail.com>
2019-06-20 23:48:06 +0300

    Вноист правки после первой проверки

--- js/script.js
+++ js/script.js
@@ -1,56 +1,19 @@
-var link = document.querySelector('.write-us-btn');
+/* открытие  попапа карты на главной */
+
 var mapLink = document.querySelector('.map-img');
-
-var popup = document.querySelector('.write-us-popup');
 var map = document.querySelector('.map-popup');
-var close = popup.querySelector(".close-cart-btn");
-var closeMap = map.querySelector(".close-cart-btn");
 
-var form = popup.querySelector("form");
-var login = popup.querySelector("[name=write_name]");
-var email = popup.querySelector("[name=write-email]");
-var text = popup.querySelector("[name=write-your-text]");
+if (map) {
+  var closeMap = map.querySelector(".close-cart-btn");
 
-mapLink.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    map.classList.add("modal-show");
-});
-
-link.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    popup.classList.add("modal-show");
-    login.focus();
-});
-
-closeMap.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    map.classList.remove("modal-show");
-});
-
-close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-    popup.classList.remove("modal-error");
-});
-
-form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-error");
-    popup.offsetWidth = popup.offsetWidth;
-    if (!login.value || !email.value || !text.value) {
+  mapLink.addEventListener('click', function(evt) {
       evt.preventDefault();
-      popup.classList.add("modal-error");
-    }
-});
+      map.classList.add("modal-show");
+  });
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
+  closeMap.addEventListener("click", function (evt) {
       evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-        popup.classList.remove("modal-error");
-      }
-    }
+      map.classList.remove("modal-show");
   });
 
   window.addEventListener("keydown", function (evt) {
@@ -61,3 +24,83 @@ window.addEventListener("keydown", function (evt) {
         }
       }
     });
+  }
+
+/* открытие  попапа напишите нам на главной */
+
+var link = document.querySelector('.write-us-btn');
+var popup = document.querySelector('.write-us-popup');
+
+if (popup) {
+  var close = popup.querySelector(".close-cart-btn");
+  var form = popup.querySelector("form");
+  var login = popup.querySelector("[name=write_name]");
+  var email = popup.querySelector("[name=write-email]");
+  var text = popup.querySelector("[name=write-your-text]");
+
+  link.addEventListener('click', function(evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-show");
+      login.focus();
+  });
+
+  close.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      if (!login.value || !email.value || !text.value) {
+        evt.preventDefault();
+        popup.classList.add("modal-error");
+      }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+      if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+          popup.classList.remove("modal-show");
+          popup.classList.remove("modal-error");
+        }
+      }
+    });
+  }
+
+/* открытие  покупки на странице каталога */
+
+var buyLink = document.querySelectorAll(".buy"), index, button;
+var cart = document.querySelector(".cart-pop-up");
+var closeCart = cart.querySelector(".close-cart-btn");
+var continueBtn = cart.querySelector(".continue-btn");
+
+  for (index = 0; index < buyLink.length; index++) {
+      button = buyLink[index];
+      button.addEventListener('click', function (event) {
+          event.preventDefault();
+          cart.classList.add("modal-show");
+      });
+  }
+
+  closeCart.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      cart.classList.remove("modal-show");
+  });
+
+  continueBtn.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      cart.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+      if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (cart.classList.contains("modal-show")) {
+          cart.classList.remove("modal-show");
+        }
+      }
+  });
